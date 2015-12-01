dbb2f847ac2d8d3cefd29aecfb093157105a6f2a
AimeeH <am.hendrycks@gmail.com>
2015-12-01 10:52:22 -0700

    import channels

--- scripts/import_json_channels.py
+++ scripts/import_json_channels.py
@@ -23,8 +23,8 @@ for channel in data:
     new_channel.artwork = channel['artwork']
     for s in channel['shows']:
         print s
-        # new_channel.show.add(Show.objects.get(show_id=s))
-        # print new_channel.show
+        new_channel.show.add(Show.objects.get(show_id=s))
+        print new_channel.show.show_id
     new_channel.upvote_count = channel['upvote_count']
     new_channel.downvote_count = channel['downvote_count']
     new_channel.save()
