b4c3f9480e19bf4eec6cbc0ac2ff9cb63766ce89
AimeeH <am.hendrycks@gmail.com>
2015-12-01 10:44:25 -0700

    import channels

--- scripts/import_json_channels.py
+++ scripts/import_json_channels.py
@@ -14,19 +14,19 @@ with open('channel_json.txt') as data_file:
 
 # print data
 for channel in data:
-        new_channel, created = Channel.objects.get_or_create(channel_id=channel['channel_id'])
-        print new_channel.channel_id
-        if channel['title'] != None:
-            new_channel.title = str(unidecode(channel['title']))
-            print new_channel.title
-        
-        new_channel.artwork = channel['artwork']
-        for s in channel['shows']:
-            new_channel.show.add(Show.objects.get(show_id=show))
-            print new_channel.show.title
-        new_channel.runtime = channel['runtime']
-        new_channel.upvote_count = channel['upvote_count']
-        new_channel.downvote_count = channel['downvote_count']
-        new_channel.save()
+    new_channel, created = Channel.objects.get_or_create(channel_id=channel['channel_id'])
+    print new_channel.channel_id
+    if channel['title'] != None:
+        new_channel.title = str(unidecode(channel['title']))
+        print new_channel.title
+    
+    new_channel.artwork = channel['artwork']
+    for s in channel['shows']:
+        new_channel.show.add(Show.objects.get(show_id=show))
+        print new_channel.show.title
+    new_channel.runtime = channel['runtime']
+    new_channel.upvote_count = channel['upvote_count']
+    new_channel.downvote_count = channel['downvote_count']
+    new_channel.save()
 
     print '<<' + str(new_channel.pk) +'>>'
