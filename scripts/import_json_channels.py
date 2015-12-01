7869188c9114485280468f5554258f8351066471
AimeeH <am.hendrycks@gmail.com>
2015-12-01 10:40:34 -0700

    import channels

---
+++ scripts/import_json_channels.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+import sys, os, pprint, json
+sys.path.append("..")  
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings")
+from unidecode import unidecode
+
+import django
+django.setup()
+from django.conf import settings
+from main.models import Channel, Show
+
+with open('channel_json.txt') as data_file:
+    data = json.load(data_file)
+
+# print data
+for channel in data:
+    try:
+        new_channel, created = Channel.objects.get_or_create(channel_id=channel['channel_id'])
+        print new_channel.channel_id
+        if channel['title'] != None:
+            new_channel.title = str(unidecode(channel['title']))
+            print new_channel.title
+        
+        new_channel.artwork = channel['artwork']
+        for s in channel['shows']:
+            new_channel.show.add(Show.objects.get(show_id=show))
+        new_channel.runtime = channel['runtime']
+        new_channel.upvote_count = channel['upvote_count']
+        new_channel.downvote_count = channel['downvote_count']
+        new_channel.save()
+    except:
+        print 'Nonetype'
+    print '<<' + str(new_channel.pk) +'>>'
