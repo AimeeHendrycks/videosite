a81bfd41619001411a6ff0b316cd4cab58714152
AimeeH <am.hendrycks@gmail.com>
2015-12-02 11:11:29 -0700

    votes

---
+++ scripts/import_json_votes.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+import sys, os, pprint, json
+sys.path.append("..")  
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings")
+from unidecode import unidecode
+
+import django
+django.setup()
+from django.conf import settings
+from main.models import Vote, Movie, Show, Channel, Episode
+
+with open('videosource_json.txt') as data_file:
+    data = json.load(data_file)
+inc = 0
+# print data
+for vote in data:
+    inc += 1
+    print inc
+
+    new_vote, created = Vote.objects.get_or_create(pk=vote['vote_pk'])
+    new_vote.vote_type = str(unidecode(vote['vote_type']))
+    if vote['display_name'] != None:
+        new_vote.display_name = str(unidecode(vote['display_name']))
+        print new_vote.display_name
+    if vote['video_kind'] == 'show':
+        new_vote.show = Show.objects.get(pk=vote['show'])
+    if vote['video_kind'] == 'movie':
+        new_vote.movie = Movie.objects.get(pk=vote['movie'])
+    if vote['video_kind'] == 'episode':
+        new_vote.episode = Episode.objects.get(pk=vote['episode'])
+    if vote['video_kind'] == 'channel':
+        new_vote.channel = Channel.objects.get(pk=vote['channel'])
+   
+    new_videosource.save()
+    
+    print '<<' + str(new_videosource.pk) +'>>'
